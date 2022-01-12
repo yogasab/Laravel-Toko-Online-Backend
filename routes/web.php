4920026e95d26afa6a7a6e627dfea4087b028290
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-12 22:34:42 +0700

    Create migration table for products, product_galleries, transactions, and transaction_details

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'Web\Dashboard\DashboardController@index')->name('dashboard.index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
