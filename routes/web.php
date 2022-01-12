d8d7460a969c82b4b59bffe23291f27c26241ef8
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-12 20:44:05 +0700

    Create admin dashboard using Dashboard Controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'Web\Dashboard\DashboardController@index')->name('dashboard.index');
